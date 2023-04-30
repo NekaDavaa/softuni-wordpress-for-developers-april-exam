11e7d5ac65aceab606fdfd4bf17ad5510fe36193
NekaDavaa <pavlov@webtik.bg>
2023-04-30 11:59:31 +0300

    Plugin created

---
+++ wp-content/plugins/softuni-rent/softuni-rent.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Plugin Name:       SoftUni Rent
+ * Plugin URI:        https://softuni.bg
+ * Description:       My exam plugin for SoftUni Rent
+ * Version:           0.1
+ * Requires at least: 5
+ * Requires PHP:      8.0
+ * Author:            Antonio Pavlov
+ * Author URI:        https://softuni.bg
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       softuni-rent
+ * Domain Path:       /languages
+ */
+
+ // Load Custom Post Types
+// require 'cpt-jobs.php';
+
+// Load our basic functions
+include 'functions.php';
